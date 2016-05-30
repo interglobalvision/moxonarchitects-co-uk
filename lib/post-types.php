ec3a7ccf9567a0459c7d7816b684f976a09260f8
Patrick Best <patrickbest@patrickbest.com>
2016-05-30 17:10:01 -0500

    register people post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -14,6 +14,7 @@ function add_menu_icons_styles(){
 }
 add_action( 'admin_head', 'add_menu_icons_styles' );
 
+
 // Register Custom Post Type
 function project_post_type() {
 
@@ -68,6 +69,7 @@ function project_post_type() {
 }
 add_action( 'init', 'project_post_type', 0 );
 
+
 // Register Custom Taxonomy
 function project_type_taxonomy() {
 
@@ -113,3 +115,58 @@ function project_type_taxonomy() {
 
 }
 add_action( 'init', 'project_type_taxonomy', 0 );
+
+
+// Register People
+function people_post_type() {
+
+	$labels = array(
+		'name'                  => 'People',
+		'singular_name'         => 'People',
+		'menu_name'             => 'People',
+		'name_admin_bar'        => 'People',
+		'archives'              => 'People Archives',
+		'parent_item_colon'     => 'Parent People:',
+		'all_items'             => 'All People',
+		'add_new_item'          => 'Add New People',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New People',
+		'edit_item'             => 'Edit People',
+		'update_item'           => 'Update People',
+		'view_item'             => 'View People',
+		'search_items'          => 'Search People',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into item',
+		'uploaded_to_this_item' => 'Uploaded to this item',
+		'items_list'            => 'Items list',
+		'items_list_navigation' => 'Items list navigation',
+		'filter_items_list'     => 'Filter items list',
+	);
+	$args = array(
+		'label'                 => 'People',
+		'description'           => 'People',
+		'labels'                => $labels,
+		'supports'              => array( ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => false,
+		'rewrite'               => false,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'people', $args );
+
+}
+add_action( 'init', 'people_post_type', 0 );
