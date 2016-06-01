98d6939914b0ffc5b6f5b331f5cbbc279d96a962
Patrick Best <patrickbest@patrickbest.com>
2016-06-01 15:52:57 -0500

    add single post layout

--- single.php
+++ single.php
@@ -2,33 +2,22 @@
 get_header();
 
   if (!is_home()) {
+    if( have_posts() ) {
+      while( have_posts() ) {
+        the_post();
 ?>
     <main id="main-content" class="menu-column menu-active">
       <div class="menu-column-top font-uppercase">
-        Posts
+        <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
       </div>
       <nav class="menu-column-content menu-background">
         <section id="posts">
 
-      <?php
-      if( have_posts() ) {
-        while( have_posts() ) {
-          the_post();
-      ?>
-
           <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-            <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+            <?php the_content(); ?>
 
           </article>
-
-      <?php
-        }
-      } else {
-      ?>
-          <article class="u-alert"><?php _e('Sorry, no posts matched your criteria'); ?></article>
-      <?php
-      } ?>
 
         </section>
       </nav>
@@ -36,6 +25,8 @@ get_header();
 
   </div>
 <?php
+      }
+    }
   }
 ?>
 
