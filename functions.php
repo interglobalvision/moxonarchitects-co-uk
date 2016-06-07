efb7327bef6ff17b1e0ac1c1b6a40f09612c3f3d
cas <casska@gmail.com>
2016-06-07 18:50:53 -0500

    fix cmb2-field-gallery include

--- functions.php
+++ functions.php
@@ -57,11 +57,18 @@ get_template_part( 'lib/theme-options' );
 
 add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
 function cmb_initialize_cmb_meta_boxes() {
-  if (!class_exists( 'cmb2_bootstrap_202' ) ) {
+  // Add CMB2 plugin
+  if( ! class_exists( 'cmb2_bootstrap_202' ) )
     require_once 'lib/CMB2/init.php';
+
+  // Add CMB2 Gallery field
+  if ( ! function_exists( 'pw_gallery_field' ) ) {
+    define( 'PW_GALLERY_URL', get_stylesheet_directory_uri() . '/lib/cmb-field-gallery/' );
     require_once 'lib/cmb-field-gallery/cmb-field-gallery.php';
   }
+
 }
+
 
 // Add custom functions
 
