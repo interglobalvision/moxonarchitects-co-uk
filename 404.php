cfd01954c398d3297a57276e79c0e5e92490865f
Patrick Best <patrickbest@patrickbest.com>
2016-06-07 17:46:47 -0500

    scaffold 404

--- 404.php
+++ 404.php
@@ -1,14 +1,38 @@
 <?php
 get_header();
 ?>
+    <main id="main-content" class="menu-column menu-active">
+      <div class="menu-column-top font-uppercase">
+        404
+      </div>
+      <nav class="menu-column-content menu-background">
+        <section id="posts">
 
-<!-- main content -->
+      <?php
+      if( have_posts() ) {
+        while( have_posts() ) {
+          the_post();
+      ?>
 
-<section id="main-content">
-  <h1>Not found</h1>
-  <p>:(</p>
-<!-- end main-content -->
-</section>
+          <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+            <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+
+          </article>
+
+      <?php
+        }
+      } else {
+      ?>
+          <article class="u-alert"><?php _e('Sorry, nothing matched your criteria'); ?></article>
+      <?php
+      } ?>
+
+        </section>
+      </nav>
+    </main>
+
+  </div>
 
 <?php
 get_footer();
