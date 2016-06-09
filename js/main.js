3fde3e7025064bf67c7c846d931133e431636b48
Patrick Best <patrickbest@patrickbest.com>
2016-06-09 18:23:12 -0500

    move script to main.js Site.Map object

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
-/* global $, jQuery, document, Site, Modernizr, Swiper */
+/* global $, jQuery, document, Site, WP, Modernizr, Swiper, google */
 
 var basicAnimationSpeed = 400;
 
@@ -14,6 +14,10 @@ Site = {
 
     _this.Menus.init();
     _this.Gallery.init();
+
+    if ($('body').hasClass('page-contact')) {
+      _this.Map.init();
+    }
 
   },
 
@@ -81,6 +85,83 @@ Site.Gallery = {
       });
 
     }
+
+  },
+};
+
+Site.Map = {
+  init: function() {
+
+    // Asynchronously Load the map API
+    var script = document.createElement('script');
+    script.src = "http://maps.googleapis.com/maps/api/js?&callback=Site.Map.initialize";
+    document.body.appendChild(script);
+
+  },
+
+  initialize: function() {
+
+    var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+      mapTypeId: 'satellite',
+      panControl: false,
+      zoomControl: false,
+      mapTypeControl: false,
+      scaleControl: false,
+      streetViewControl: false,
+      overviewMapControl: false,
+      draggable: true,
+      disableDoubleClickZoom: false,
+      scrollwheel: true,
+    };
+
+    // Display a map on the page
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    map.setTilt(45);
+
+    // Multiple Markers
+    var markers = [
+      ['London', 51.521351,-0.196285],
+      ['Scotland', 57.0434123,-3.2153803]
+    ];
+
+    // Info Window Content
+    var infoWindowContent = [
+      ['<div class="info_content">' + WP.mapData.London + '</div>'],
+      ['<div class="info_content">' + WP.mapData.Scotland + '</div>'],
+    ];
+
+    // Display multiple markers on a map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+
+    // Loop through our array of markers & place each one on the map
+    for( i = 0; i < markers.length; i++ ) {
+      var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+      bounds.extend(position);
+      marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: markers[i][0]
+      });
+
+      // Allow each marker to have an info window
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infoWindow.setContent(infoWindowContent[i][0]);
+          infoWindow.open(map, marker);
+        };
+      })(marker, i));
+
+      // Automatically center the map fitting all markers on the screen
+      map.fitBounds(bounds);
+    }
+
+    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+      this.setZoom(6);
+      google.maps.event.removeListener(boundsListener);
+    });
 
   },
 };
