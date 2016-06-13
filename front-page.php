8563908a8858d6682fdc0b925ed8012166f86467
Patrick Best <patrickbest@patrickbest.com>
2016-06-13 17:48:34 -0500

    scaffold front page

---
+++ front-page.php
@@ -0,0 +1,27 @@
+<?php
+get_header();
+
+$front_page_projects = IGV_get_option('_igv_front_page');
+$front_page_projects = preg_split('/[\s,]+/', $front_page_projects);
+
+$random_index = rand(0, (count($front_page_projects) - 1));
+
+$front_page_random_project = get_post($front_page_projects[$random_index]);
+
+$front_page_gallery = get_post_meta($front_page_projects[$random_index], '_igv_gallery');
+?>
+  </div>
+
+  <a href="<?php echo get_the_permalink($front_page_projects[$random_index]); ?>">
+  <section id="images" class="u-flex-center">
+    <?php
+      if (!empty($front_page_gallery)) {
+        render_gallery($front_page_gallery);
+      }
+    ?>
+  </section>
+  </a>
+
+<?php
+get_footer();
+?>
