a256ea0bc700ce90de5437d48a327f671a5cde16
Patrick Best <patrickbest@patrickbest.com>
2016-07-05 00:42:08 +0100

    add project type archive
    as per mobile design but works on desktop. not directly accessable in
    the desktop version

---
+++ taxonomy-project_type.php
@@ -0,0 +1,52 @@
+<?php
+get_header();
+
+  if (!is_home()) {
+?>
+    <main id="main-content" class="menu-column menu-active">
+      <div class="menu-column-top font-uppercase">
+        &nbsp;
+      </div>
+      <nav class="menu-column-content menu-background">
+        <section id="posts">
+
+      <?php
+      if( have_posts() ) {
+        while( have_posts() ) {
+          the_post();
+      ?>
+          <a href="<?php the_permalink() ?>">
+            <article <?php post_class('project-type-archive-post'); ?> id="post-<?php the_ID(); ?>">
+
+              <?php the_post_thumbnail('project-type-archive'); ?>
+
+              <div class="project-type-archive-post-title-holder">
+                <h4 class="project-type-archive-post-title font-uppercase"><?php the_title(); ?></h4>
+              </div>
+
+            </article>
+          </a>
+
+      <?php
+        }
+      } else {
+      ?>
+          <article class="u-alert"><?php _e('Sorry, no posts matched your criteria'); ?></article>
+      <?php
+      } ?>
+
+        </section>
+      </nav>
+    </main>
+
+  </div>
+<?php
+  }
+?>
+
+  <section id="images" class="u-flex-center">
+  </section>
+
+<?php
+get_footer();
+?>
