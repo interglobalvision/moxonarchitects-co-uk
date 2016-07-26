93227abc097807c5b1b7525e24efba9b430bdc62
Patrick Best <patrickbest@patrickbest.com>
2016-07-26 15:19:44 -0500

    fix index.php for news posts

--- index.php
+++ index.php
@@ -1,46 +1,40 @@
 <?php
 get_header();
-
-  if (!is_home()) {
 ?>
-    <main id="main-content" class="menu-column menu-active">
-      <div class="menu-column-top font-uppercase">
-        Posts
-      </div>
-      <nav class="menu-column-content menu-background">
-        <section id="posts">
-
-      <?php
-      if( have_posts() ) {
-        while( have_posts() ) {
-          the_post();
-      ?>
-
-          <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-
-            <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-
-          </article>
-
-      <?php
-        }
-      } else {
-      ?>
-          <article class="u-alert"><?php _e('Sorry, no posts matched your criteria'); ?></article>
-      <?php
-      } ?>
-
-        </section>
-      </nav>
-    </main>
-
   </div>
 <?php
+
+  if (!is_front_page()) {
+    if( have_posts() ) {
+    ?>
+    <section id="news">
+    <?php
+      while( have_posts() ) {
+        the_post();
+    ?>
+
+        <article <?php post_class('news-post'); ?> id="post-<?php the_ID(); ?>">
+
+          <?php the_post_thumbnail('news-thumb'); ?>
+          <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+
+        </article>
+
+    <?php
+      }
+    ?>
+    </section>
+    <?php
+    } else {
+    ?>
+    <section id="news">
+      <article class="u-alert"><?php _e('Sorry, no posts matched your criteria'); ?></article>
+    </section>
+    <?php
+    }
   }
 ?>
 
-  <section id="images" class="u-flex-center">
-  </section>
 
 <?php
 get_footer();
