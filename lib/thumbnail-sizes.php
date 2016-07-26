910fc92fafd5a578e656aa294c2e09f32c0fe923
Patrick Best <patrickbest@patrickbest.com>
2016-07-26 15:20:25 -0500

    add news image thumb size

--- lib/thumbnail-sizes.php
+++ lib/thumbnail-sizes.php
@@ -11,4 +11,6 @@ if( function_exists( 'add_image_size' ) ) {
   add_image_size( 'gallery', 1200, 9999, false );
   add_image_size( 'mobile-project-image', 800, 9999, false );
   add_image_size( 'project-type-archive', 483, 278, true );
+
+  add_image_size( 'news-thumb', 1000, 9999, false );
 }
