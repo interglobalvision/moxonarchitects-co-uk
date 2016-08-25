b26c6bdfcb86c9849c03328313b621defbbfa2e6
Patrick Best <patrickbest@patrickbest.com>
2016-08-25 13:47:03 -0500

    add bigger sizes for gallery images

--- lib/functions-custom.php
+++ lib/functions-custom.php
@@ -18,7 +18,15 @@ function render_gallery($gallery) {
 ?>
 <div id="swiper-gallery" class="swiper-container">
   <div class="swiper-wrapper">
-<?php foreach ($gallery[0] as $slide) { ?><div class="swiper-slide"><?php echo wp_get_attachment_image($slide, 'gallery', null, array('class' => 'gallery-image')); ?></div><?php } ?>
+<?php
+  foreach ($gallery[0] as $slide) {
+?>
+    <div class="swiper-slide">
+      <?php echo wp_get_attachment_image($slide, 'gallery', null, array('class' => 'gallery-image gallery-image-normal')); ?>
+      <?php echo wp_get_attachment_image($slide, 'gallery-large', null, array('class' => 'gallery-image gallery-image-large')); ?>
+      <?php echo wp_get_attachment_image($slide, 'gallery-huge', null, array('class' => 'gallery-image gallery-image-huge')); ?>
+    </div>
+<?php } ?>
   </div>
 </div>
 <?php
