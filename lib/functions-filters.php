eb20bcd59a63804e3a97d7b4c49967635a0e9924
Patrick Best <patrickbest@patrickbest.com>
2016-08-29 14:41:07 -0500

    update lazysizes php filter

--- lib/functions-filters.php
+++ lib/functions-filters.php
@@ -23,22 +23,27 @@ function filter_ptags_on_iframes($content){
 }
 add_filter('the_content', 'filter_ptags_on_iframes');
 
-/* 
- * Custom img attributes to be compatible with lazysize
- */
+// Custom img attributes to be compatible with lazysize
 function add_lazysize_on_srcset($attr) {
+
   // Add lazysize class
   $attr['class'] .= ' lazyload';
 
-  // Add lazysize data-srcset
-  $attr['data-srcset'] = $attr['srcset'];
-
-  // Remove default srcset
-  unset($attr['srcset']);
-
-  // Remove default src
-  unset($attr['src']);
+  if (isset($attr['srcset'])) {
+    // Add lazysize data-srcset
+    $attr['data-srcset'] = $attr['srcset'];
+    // Remove default srcset
+    unset($attr['srcset']);
+    // Remove default src
+    unset($attr['src']);
+  } else {
+    // Add lazysize data-src
+    $attr['data-src'] = $attr['src'];
+    // Remove default src
+    unset($attr['src']);
+  }
 
   return $attr;
+
 }
 add_filter('wp_get_attachment_image_attributes', 'add_lazysize_on_srcset');
