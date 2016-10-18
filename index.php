b2497dc0eefce991f2a1d0c18aa2e71718c8a82a
Patrick Best <patrickbest@patrickbest.com>
2016-10-18 17:15:13 -0500

    remove day from news date

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ get_header();
 
           <div class="news-post-content">
             <h2 class="news-post-title margin-bottom-tiny text-align-center js-fix-widows"><?php the_title(); ?></h2>
-            <div class="text-align-center font-copy font-uppercase margin-bottom-basic"><?php the_time('jS F Y'); ?></div>
+            <div class="text-align-center font-copy font-uppercase margin-bottom-basic"><?php the_time('F Y'); ?></div>
             <div class="font-copy">
               <?php the_content(); ?>
             </div>
