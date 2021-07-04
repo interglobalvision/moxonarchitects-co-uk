dedd16f153ced74e0cd0c650b96bf0720643dfad
varffs <patrickbest@patrickbest.com>
2021-07-04 20:32:26 +0100

    add video metabox scaffold

--- lib/meta-boxes.php
+++ lib/meta-boxes.php
@@ -79,6 +79,32 @@ function igv_cmb_metaboxes() {
     'sanitization_cb' => 'pw_gallery_field_sanitise',
     'preview_size' => array( 150, 150 ),
   ) );
+  
+  $project_videos = $project_meta->add_field( array(
+    'id'          => $prefix . 'project_videos',
+    'type'        => 'group',
+    'description' => __( 'Adds videos to the gallery at the specified index', 'cmb2' ),
+    'options'     => array(
+      'group_title'       => __( 'Video {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
+      'add_button'        => __( 'Add Another Video', 'cmb2' ),
+      'remove_button'     => __( 'Remove Video', 'cmb2' ),
+      'sortable'          => false,
+    ),
+  ) );
+
+  // Id's for group's fields only need to be unique for the group. Prefix is not needed.
+  $project_meta->add_group_field( $project_videos, array(
+    'name' => 'Video Index',
+    'description' => 'The position relative to the gallery images to show the video. 1 is before any images. 3 is after the 2nd image. etc',
+    'id'   => 'index',
+    'type' => 'text',
+  ) );
+  
+  $project_meta->add_group_field( $project_videos, array(
+    'name' => 'Video',
+    'id'   => 'video',
+    'type' => 'file',
+  ) );
 
   $contact_page_meta = new_cmb2_box( array(
     'id'            => $prefix . 'contact_page_metabox',
